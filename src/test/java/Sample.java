fd44f4c489dc2b5fca38604f45d5e3083a121b43
day <laasiya.sivanandam@gmail.com>
2024-09-14 11:34:37 +0530

    code

---
+++ src/test/java/Sample.java
@@ -0,0 +1,15 @@
+
+public class Sample {
+private void method1() {
+	System.out.println("method1");
+
+}
+private void method2() {
+	System.out.println("method1");
+
+}
+private void method3() {
+	System.out.println("method1");
+
+}
+}
